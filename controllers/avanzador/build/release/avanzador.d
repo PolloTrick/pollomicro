1f7b338b963237521895db491d807ef8cba155a0
Tu Nombre <tucorreo@gmail.com>
2026-09-18 20:09:42 -0600

    controladores

---
+++ controllers/avanzador/build/release/avanzador.d
@@ -0,0 +1,5 @@
+build/release/avanzador.o: avanzador.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp
---
+++ controllers/c_pendulo/build/release/c_pendulo.d
@@ -0,0 +1,6 @@
+build/release/c_pendulo.o: c_pendulo.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
